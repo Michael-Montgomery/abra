565919c2019ae1dc76644b76160410ccf757c232
michael-montgomery <thehealthyprogrammer@outlook.com>
2024-10-22 08:54:49 -0400

    swapped browser for hasrouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/home';
 import Shows from './pages/shows/shows';
 import Contact from './pages/contact/contact';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename='/'>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/events' element={<Shows></Shows>}></Route>
         <Route path='/contact' element={<Contact></Contact>}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
